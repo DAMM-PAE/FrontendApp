614b88b601f1821115c0698bd4e5b190ea6978bf
lucas.ares <lucas.ares@estudiantat.upc.edu>
2023-12-10 19:58:49 +0100

    App conectada a Mastodon

---
+++ damm_app/lib/ayuda.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.red,
+      ),
+      home: HelpScreen(),
+    );
+  }
+}
+
+class HelpScreen extends StatelessWidget {
+  final TextEditingController messageController = TextEditingController();
+
+  Future<void> sendEmail(String message, BuildContext context) async {
+    final Email email = Email(
+      body: message,
+      subject: 'Ayuda desde la aplicación Damm App',
+      recipients: ['kekoneh469@notedns.com'],
+      isHTML: false,
+    );
+
+    try {
+      await FlutterEmailSender.send(email);
+      // Muestra un mensaje de éxito
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Mensaje Enviado'),
+            content: Text('Su mensaje ha sido enviado con éxito.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } catch (error) {
+      // Muestra un mensaje de error
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text('Hubo un problema al enviar su mensaje. Inténtelo de nuevo más tarde.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Ayuda'),
+      ),
+    body: SingleChildScrollView(
+      child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Escriba sus preguntas o sugerencias:',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 50),
+                Text(
+                  'Escriba su mensaje y presione "Enviar" para que su comentario llegue a nuestro equipo de soporte.',
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 40),
+                TextField(
+                  controller: messageController,
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    labelText: 'Tu mensaje',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 200),
+                ElevatedButton(
+                  onPressed: () {
+                    String message = messageController.text;
+                    if (message.isNotEmpty) {
+                      sendEmail(message, context);
+                    } else {
+                      // Muestra un mensaje de error si el mensaje está vacío
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('Error'),
+                            content: Text('Por favor, escribe tu mensaje antes de enviarlo.'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    foregroundColor: Colors.white,
+                    minimumSize: Size(200, 60),
+                  ),
+                  child: Text(
+                    'Enviar',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ damm_app/lib/home_screen.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:damm_app/login.dart';
+import 'package:damm_app/ayuda.dart';
+
+class HomeScreen extends StatelessWidget {
+  final String barId;
+  final String fechaProximaEntrega;
+  final String porcentajeCerveza;
+  final String nombreDelBar;
+
+  HomeScreen({
+    required this.barId,
+    required this.fechaProximaEntrega,
+    required this.porcentajeCerveza,
+    required this.nombreDelBar,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Damm App'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              child: Text(
+                nombreDelBar,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Ayuda'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => HelpScreen()));
+              },
+            ),
+            ListTile(
+              title: Text(
+                'Cerrar Sesión',
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+              onTap: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => LoginScreen(),
+                  ),
+                      (route) => false,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 100),
+              Text(
+                nombreDelBar,
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 200),
+              Text(
+                'Fecha de la próxima entrega:',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                '$fechaProximaEntrega',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 40),
+              Text(
+                'Porcentaje de cerveza restante:',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                '$porcentajeCerveza %',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 80),
+              ElevatedButton(
+                onPressed: () {
+                  // Implementa la lógica para realizar el pedido
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(200, 60),
+                ),
+                child: Text(
+                  'Pedir Ya',
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
